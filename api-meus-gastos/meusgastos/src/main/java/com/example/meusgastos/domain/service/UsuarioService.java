067fd90bde44a1730cb84b7980d58ddff1ccd940
kcarww <carlos.oe.cc@gmail.com>
2025-01-25 11:25:27 -0300

    feat: UsuarioService findall

--- api-meus-gastos/meusgastos/src/main/java/com/example/meusgastos/domain/service/UsuarioService.java
+++ api-meus-gastos/meusgastos/src/main/java/com/example/meusgastos/domain/service/UsuarioService.java
@@ -1,20 +1,44 @@
 package com.example.meusgastos.domain.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.meusgastos.domain.model.Usuario;
+import com.example.meusgastos.domain.repository.UsuarioRepository;
 import com.example.meusgastos.dto.usuario.UsuarioRequestDto;
 import com.example.meusgastos.dto.usuario.UsuarioResponseDto;
 
 public class UsuarioService implements ICRUDService<UsuarioRequestDto, UsuarioResponseDto>{
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
     public List<UsuarioResponseDto> obterTodos() {
-        throw new UnsupportedOperationException("Unimplemented method 'obterTodos'");
+        List<Usuario> usuarios = usuarioRepository.findAll();
+
+        return usuarios.stream()
+            .map(usuario -> mapper.map(usuario, UsuarioResponseDto.class))
+            .collect(Collectors.toList());
     }
 
     @Override
     public UsuarioResponseDto obterPorId(Long id) {
-        throw new UnsupportedOperationException("Unimplemented method 'obterPorId'");
+        Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+        if (usuario.isPresent()) {
+
+        }
+
+        return mapper.map(usuario.get(), UsuarioResponseDto.class);
     }
 
     @Override
